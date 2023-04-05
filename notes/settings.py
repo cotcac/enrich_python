6b92119a147908b680d2b863aa6ef95480ae9c63
Vuong <vuong@vuong.com>
2023-04-05 19:30:56 +0700

    use postgres db

--- notes/settings.py
+++ notes/settings.py
@@ -83,6 +83,18 @@ DATABASES = {
     }
 }
 
+# config use postgres
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'notedb',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
--- topics/models.py
+++ topics/models.py
@@ -1,4 +1,4 @@
 from django.db import models
 
 class Topics(models.Model):
-    title = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
